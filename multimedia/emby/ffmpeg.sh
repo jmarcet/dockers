9f447c6ed213fc0533d1d2c34f84be40dcb08f00
Javier Marcet <javier@marcet.info>
2021-09-12 23:10:04 +0200

    multimedia: drop emby
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- multimedia/emby/ffmpeg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if echo $@ | grep -q h264_vaapi; then
-	ARGS=$@
-else
-	ARGS="$( echo $@ | sed -E -e 's,c:v:0 h264 -user,c:v:0 h264 -hwaccel:v:0 vaapi -hwaccel_device:v:0 /dev/dri/renderD128 -hwaccel_output_format:v:0 vaapi -user,' -e 's: libx264: h264_vaapi:' -e 's,filter_complex [^ ]+ ,filter_complex [0:0]deinterlace_vaapi@f1[f1_out0] ,' -e 's, -pix_fmt:v:0 .+ -profile, -profile,' -e 's, -x264opts:v:0 .+ -c:a:0, -c:a:0,' )"
-fi
-ffmpeg $ARGS
